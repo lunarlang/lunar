bc6232ce8595ea068cb6455719b0e3d77c68919f
Alexander <krontimus@gmail.com>
2018-12-17 19:22:57 -0500

    add tokeninfo class

---
+++ lunar/compiler/lexical/token_info.lua
@@ -0,0 +1,15 @@
+local TokenInfo = {}
+TokenInfo.__index = TokenInfo
+
+function TokenInfo.new(token_type, value, position)
+  local self = setmetatable({}, TokenInfo)
+  self.token_type = token_type
+  self.value = value
+  self.position = position
+
+  return self
+end
+
+function TokenInfo:__tostring()
+  return ("%d %s"):format(self.token_type, self.value)
+end
---
+++ lunar/compiler/lexical/token_type.lua
@@ -0,0 +1,3 @@
+return {
+  none = 0,
+}
