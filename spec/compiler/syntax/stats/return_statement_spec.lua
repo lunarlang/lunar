02771f4e96601c20f08051e0cc80c37f6ebcf889
Alexander <krontimus@gmail.com>
2019-01-19 17:17:01 -0500

    update ReturnStatement pending cases to test expressions list

--- spec/compiler/syntax/stats/return_statement_spec.lua
+++ spec/compiler/syntax/stats/return_statement_spec.lua
@@ -1,6 +1,7 @@
 local Lexer = require "lunar.compiler.lexical.lexer"
 local Parser = require "lunar.compiler.syntax.parser"
 local ReturnStatement = require "lunar.ast.stats.return_statement"
+local NilLiteralExpression = require "lunar.ast.exprs.nil_literal_expression"
 
 describe("ReturnStatement syntax", function()
   it("should only return one ReturnStatement node", function()
@@ -12,7 +13,21 @@ describe("ReturnStatement syntax", function()
     }, ast)
   end)
 
-  pending("should return one ReturnStatement node with one expression")
+  it("should return one ReturnStatement node with one expression", function()
+    local tokens = Lexer.new("return nil"):tokenize()
+    local ast = Parser.new(tokens):parse()
 
-  pending("should return one ReturnStatement node with two expressions")
+    assert.same({
+      ReturnStatement.new(NilLiteralExpression.new())
+    }, ast)
+  end)
+
+  it("should return one ReturnStatement node with two expressions", function()
+    local tokens = Lexer.new("return nil, nil"):tokenize()
+    local ast = Parser.new(tokens):parse()
+
+    assert.same({
+      ReturnStatement.new(NilLiteralExpression.new(), NilLiteralExpression.new())
+    }, ast)
+  end)
 end)
