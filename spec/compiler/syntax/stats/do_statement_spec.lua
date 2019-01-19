583c5b8945ca0d4ca905fb2c4cfa7bdf28f29e42
Alexander <krontimus@gmail.com>
2019-01-18 20:34:40 -0500

    add tests for do statement syntax

---
+++ spec/compiler/syntax/stats/do_statement_spec.lua
@@ -0,0 +1,33 @@
+local Lexer = require "lunar.compiler.lexical.lexer"
+local Parser = require "lunar.compiler.syntax.parser"
+local DoStatement = require "lunar.ast.stats.do_statement"
+
+describe("DoStatement syntax", function()
+  it("should parse one DoStatement node", function()
+    local tokens = Lexer.new("do end"):tokenize()
+    local ast = Parser.new(tokens):parse()
+
+    assert.same({
+      DoStatement.new()
+    }, ast)
+  end)
+
+  it("should parse two DoStatement nodes", function()
+    local tokens = Lexer.new("do end do end"):tokenize()
+    local ast = Parser.new(tokens):parse()
+
+    assert.same({
+      DoStatement.new(),
+      DoStatement.new()
+    }, ast)
+  end)
+
+  it("should parse nested DoStatement nodes", function()
+    local tokens = Lexer.new("do do do end end end"):tokenize() -- de do do do de da da da
+    local ast = Parser.new(tokens):parse()
+
+    assert.same({
+      DoStatement.new(DoStatement.new(DoStatement.new()))
+    }, ast)
+  end)
+end)
