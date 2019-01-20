fbf1fd990482ac1b1f982f7b5fb55f2cd91ced00
Alexander <krontimus@gmail.com>
2019-01-20 11:38:26 -0500

    add NilLiteralExpression test case

---
+++ spec/compiler/syntax/exprs/nil_literal_expresssion_spec.lua
@@ -0,0 +1,12 @@
+local Lexer = require "lunar.compiler.lexical.lexer"
+local Parser = require "lunar.compiler.syntax.parser"
+local NilLiteralExpression = require "lunar.ast.exprs.nil_literal_expression"
+
+describe("NilLiteralExpression syntax", function()
+  it("should return one NilLiteralExpression node", function()
+    local tokens = Lexer.new("nil"):tokenize()
+    local ast = Parser.new(tokens):parse_expression()
+
+    assert.same(NilLiteralExpression.new(), ast)
+  end)
+end)
