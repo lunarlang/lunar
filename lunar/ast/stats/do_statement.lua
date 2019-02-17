391711c027320a544d054aeafc7e0207c87dceda
Alexander <krontimus@gmail.com>
2019-02-16 22:17:43 -0500

    remove useless block variable that simply aliased to stats parameter

--- lunar/ast/stats/do_statement.lua
+++ lunar/ast/stats/do_statement.lua
@@ -4,9 +4,7 @@ local SyntaxNode = require "lunar.ast.syntax_node"
 local DoStatement = setmetatable({}, SyntaxNode)
 DoStatement.__index = DoStatement
 
-function DoStatement.new(stats)
-  local block = stats
-
+function DoStatement.new(block)
   local super = SyntaxNode.new(SyntaxKind.do_statement)
   local self = setmetatable(super, DoStatement)
   self.block = block
