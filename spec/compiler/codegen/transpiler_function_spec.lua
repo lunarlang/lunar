351d13716efde1d67e54913b8581bf88f442fa16
Alexander <krontimus@gmail.com>
2019-02-23 00:17:36 -0500

    refactor function_spec to call the function inside the input

--- spec/compiler/codegen/transpiler_function_spec.lua
+++ spec/compiler/codegen/transpiler_function_spec.lua
@@ -17,7 +17,7 @@ describe("FunctionStatement transpilation", function()
   end)
 
   it("should support MemberExpression function using dot notation", function()
-    local input = "function a.b(c) return c end"
+    local input = "function a.b(c) return c end; return a.b(1), a.b(2)"
 
     local tokens = Lexer.new(input):tokenize()
     local ast = Parser.new(tokens):parse()
@@ -26,12 +26,12 @@ describe("FunctionStatement transpilation", function()
     local a = {}
     local program = Program.new(result, { a = a }):run()
 
-    assert.equal(1, program.env.a.b(1))
-    assert.equal(2, program.env.a.b(2))
+    assert.equal(1, program.result[1])
+    assert.equal(2, program.result[2])
   end)
 
   it("should support MemberExpression function using colon notation", function()
-    local input = "function a:b() return self end"
+    local input = "function a:b() return self end; return a:b()"
 
     local tokens = Lexer.new(input):tokenize()
     local ast = Parser.new(tokens):parse()
@@ -40,31 +40,26 @@ describe("FunctionStatement transpilation", function()
     local a = {}
     local program = Program.new(result, { a = a }):run()
 
-    assert.equal(program.env.a, program.env.a:b())
+    assert.equal(program.env.a, program.result[1])
   end)
 
   it("should support varargs", function()
-    local input = "function a(...) return ... end"
+    local input = "function a(...) return {...} end; return a(1, 2), a(4, 'abc', false)"
 
     local tokens = Lexer.new(input):tokenize()
     local ast = Parser.new(tokens):parse()
     local result = Transpiler.new(ast):transpile()
 
     local program = Program.new(result):run()
-    spy.on(program.env, "a")
 
-    local r = { program.env.a(1, 2) }
-    assert.spy(program.env.a).was.called_with(1, 2)
-    assert.equal(2, #r)
-    assert.equal(1, r[1])
-    assert.equal(2, r[2])
+    assert.equal(2, #program.result[1])
+    assert.equal(1, program.result[1][1])
+    assert.equal(2, program.result[1][2])
 
-    local r = { program.env.a(4, "abc", false) }
-    assert.spy(program.env.a).was.called_with(4, "abc", false)
-    assert.equal(3, #r)
-    assert.equal(4, r[1])
-    assert.equal("abc", r[2])
-    assert.equal(false, r[3])
+    assert.equal(3, #program.result[2])
+    assert.equal(4, program.result[2][1])
+    assert.equal("abc", program.result[2][2])
+    assert.equal(false, program.result[2][3])
   end)
 
   it("should support function expressions", function()
