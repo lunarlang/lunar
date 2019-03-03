63a2b199b03494c34f82a41054f17b812a8abff1
Alexander <krontimus@gmail.com>
2019-03-02 20:00:35 -0500

    fix pathing issue with backslashes

--- bin/lunarc.ps1
+++ bin/lunarc.ps1
@@ -1,5 +1,7 @@
+$path = [regex]::escape($env:APPDATA)
+
 @"
-package.path = ";./?.lua;./?/init.lua;%APPDATA%/Lunar/?.lua;%APPDATA%/Lunar/?/init.lua;" .. package.path
+package.path = ";./?.lua;./?/init.lua;$path/Lunar/?.lua;$path/Lunar/?/init.lua;" .. package.path
 
 require("lunar.lunarc")
 "@ | lua
